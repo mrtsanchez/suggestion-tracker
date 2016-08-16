675af1e72105744427729170a95582214c0ed0eb
Marta Sanchez <sanchez.marta@gmail.com>
2016-08-15 18:44:24 -0700

    fix the indentation in the js and the css files

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Backend Logic
 
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 // Frontend Logic
